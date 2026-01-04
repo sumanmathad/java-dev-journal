241073d1ed1152399052f3ceb782a304baf4b021
suman.mathad <suman.mathad@oracle.com>
2026-01-04 17:38:12 +0530

    java updates

---
+++ src/main/java/dev/playground/Datatypes.java
@@ -0,0 +1,24 @@
+package dev.playground;
+
+public class Datatypes {
+    static boolean state;
+    static int j;
+    public static void main(String[] args) {
+        System.out.println(Datatypes.state);
+        //When a boolean is declared as a field (a member of a class), Java automatically initializes it to a default value.
+        //Default Value: false
+        //This applies to both instance variables (belonging to an object) and static variables (belonging to the class).
+
+        //If you declare a boolean inside a method, it is considered a local variable. Java does not provide a default value for local variables.
+
+        System.out.println(j);
+
+
+
+    }
+
+
+
+
+
+}
---
+++ src/main/java/dev/variables/Basics.java
@@ -0,0 +1,18 @@
+package dev.variables;
+
+import java.util.Arrays;
+
+public class Basics {
+
+    public static void main(String[] args) {
+        boolean s=true;
+        char c = 'a';
+
+
+        System.out.println(c);
+        System.out.print(c);
+        System.out.print(c);
+    }
+
+
+}
